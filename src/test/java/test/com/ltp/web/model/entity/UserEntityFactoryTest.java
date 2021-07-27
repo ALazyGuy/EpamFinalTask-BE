7ed4a1425e6f1e676098bf33cf4b9ecb6e9438f3
ALazyGuy <razorprogrammer@gmail.com>
2021-07-27 22:08:24 +0300

    feat: Added test for user factory

--- src/test/java/test/com/ltp/web/model/entity/UserEntityFactoryTest.java
+++ src/test/java/test/com/ltp/web/model/entity/UserEntityFactoryTest.java
@@ -1,2 +1,28 @@
-package test.com.ltp.web.model.entity;public class UserEntityFactoryTest {
+package test.com.ltp.web.model.entity;
+
+import com.ltp.web.model.entity.UserEntity;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+public class UserEntityFactoryTest {
+
+    @Test
+    public void createUserTest(){
+        UserEntity expected = new UserEntity();
+        expected.setCash(0);
+        expected.setEmail("test");
+        expected.setName("test");
+        expected.setMiddleName("test");
+        expected.setSurname("test");
+        expected.setPassword("test");
+
+        UserEntity actual = UserEntity.UserEntityFactory.createUser("test",
+                "test",
+                "test",
+                "test",
+                "test");
+
+        AssertJUnit.assertEquals(expected, actual);
+    }
+
 }
