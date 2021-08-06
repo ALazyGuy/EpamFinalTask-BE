249ff98724a83f15abfabe09a84d1a89aa3cbaef
ALazyGuy <razorprogrammer@gmail.com>
2021-08-06 21:54:56 +0300

    feat: Added listener for initialization

---
+++ src/main/java/com/ltp/web/listener/InitializationListener.java
@@ -0,0 +1,32 @@
+package com.ltp.web.listener;
+
+import com.ltp.web.connection.ConnectionPool;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.SQLException;
+
+@WebListener
+public class InitializationListener implements ServletContextListener {
+
+    private static final Logger LOGGER = LogManager.getLogger(InitializationListener.class);
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ServletContextListener.super.contextInitialized(sce);
+        ConnectionPool.getInstance();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        ServletContextListener.super.contextDestroyed(sce);
+        try {
+            ConnectionPool.getInstance().closeAll();
+        } catch (SQLException e) {
+            LOGGER.error("Unable to close connections");
+        }
+    }
+}
