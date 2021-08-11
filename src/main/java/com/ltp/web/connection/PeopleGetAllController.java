dfa54f5b06932bda14f00fcfc4a8793f5a581270
ALazyGuy <razorprogrammer@gmail.com>
2021-08-11 19:50:38 +0300

    feat: Added /people/getAll

---
+++ src/main/java/com/ltp/web/connection/PeopleGetAllController.java
@@ -0,0 +1,33 @@
+package com.ltp.web.connection;
+
+import com.ltp.web.exception.ConnectionPoolException;
+import com.ltp.web.mapper.JsonMapper;
+import com.ltp.web.model.entity.PeopleEntity;
+import com.ltp.web.service.impl.PeopleServiceImpl;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet("/people/getAll")
+public class PeopleGetAllController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            List<PeopleEntity> peopleEntities = PeopleServiceImpl.getInstance().findAll();
+            String response = JsonMapper.parseToString(peopleEntities);
+            resp.setContentType("application/json");
+            resp.getWriter().write(response);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ConnectionPoolException e) {
+            e.printStackTrace();
+        }
+    }
+}
