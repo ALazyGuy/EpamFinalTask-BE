c71f3f19db9925975869abc794cbbde8cc104733
ALazyGuy <razorprogrammer@gmail.com>
2021-08-18 00:58:02 +0300

    feat: Added registration validator

--- src/main/java/com/ltp/web/validator/AuthValidator.java
+++ src/main/java/com/ltp/web/validator/AuthValidator.java
@@ -1,6 +1,7 @@
 package com.ltp.web.validator;
 
 import com.ltp.web.model.dto.LoginRequest;
+import com.ltp.web.model.dto.RegistrationRequest;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -10,12 +11,25 @@ import java.util.regex.Pattern;
 public class AuthValidator {
 
     private static final String EMAIL_EXP = "(^\\w+@[a-zA-Z]+\\.[a-zA-Z]+$)";
-    private static final String PASSWORD_EXP = "(^(([a-zA-Z0-9]|\\.|\\$|\\^)+)$)";
+    private static final String PASSWORD_EXP = "(^((\\w|\\.|\\$|\\^){4,30})$)";
+    private static final String FULL_NAME_EXP = "(^[a-zA-Z]{2,20}$)";
 
     public static boolean validateLogin(LoginRequest loginRequest){
         boolean isEmailValid = Pattern.matches(EMAIL_EXP, loginRequest.getEmail());
         boolean isPasswordValid = Pattern.matches(PASSWORD_EXP, loginRequest.getPassword());
         return isEmailValid && isPasswordValid && loginRequest.getPassword().length() >= 4;
+    }
+
+    public static boolean validateRegistration(RegistrationRequest registrationRequest){
+        boolean isEmailValid = Pattern.matches(EMAIL_EXP, registrationRequest.getEmail());
+        boolean isPasswordValid = Pattern.matches(PASSWORD_EXP, registrationRequest.getPassword());
+        boolean isFullNameValid = Pattern.matches(FULL_NAME_EXP, registrationRequest.getName())
+                && Pattern.matches(FULL_NAME_EXP, registrationRequest.getSurname())
+                && Pattern.matches(FULL_NAME_EXP, registrationRequest.getMiddleName());
+        return isEmailValid
+                && isPasswordValid
+                && isFullNameValid
+                && registrationRequest.getPassword().length() >= 4;
     }
 
 }
